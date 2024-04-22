fafcb773381f2e57455b4700d2a064834f988ae2
t-kubota72459 <t-kubota72459@vt-school.hiroshima.jp>
2024-04-22 15:02:53 +0100

    updated

---
+++ kadai2-3.ino
@@ -0,0 +1,18 @@
+//
+// 課題 3
+//
+const int OUTPUT_PIN = 3;
+int brightness = 0;
+
+void setup(){
+    pinMode( OUTPUT_PIN, OUTPUT );
+}
+
+void loop(){
+    analogWrite( OUTPUT_PIN, XXXXXXXX );     // analogWrite() 0 ~ 255 までを指定できる
+
+    brightness = brightness + 1;
+    if (brightness == YYYYYYYYY ) {
+      birghtness = 0;
+    }
+}
